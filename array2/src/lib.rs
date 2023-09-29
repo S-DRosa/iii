d56628f663d9fd376fd3d5bf87c6dfa470b676f6
Marcin Pawlukiewicz <marcinpawlukiewicz@marcins-imac.myfiosgateway.com>
2023-09-28 22:56:08 -0400

    changes in lib.rs

--- array2/src/lib.rs
+++ array2/src/lib.rs
@@ -7,13 +7,30 @@ height: usize,
 
 impl Array2<T>{
     /*Creates an Array2 object with a width, height, and data */
-pub fn new(width: usize, height: usize)->self;
+pub fn new(width: usize, height: usize)->self{
+    //data Vec with default values
+    let data = vec![Default::default(); width * height];
+
+    Array2{
+        data,
+        width,
+        height,
+    }
+}
 
 /*This will create an Array2 where the Array2 is order ordered by rows */
-pub fn from_row_major(width: usize, height: usize)->self;
+pub fn from_row_major(data: Vec<(T, (usize, usize))>, width: usize, height: usize)->self{
+    if data.len() != width * height {
+        panic!("Data length not equal to width * height");
+    }
+}
 
 /*This will create an Array2 where the Array2 is ordered by column */
-pub fn from_column_major(width: usize, height: usize)->self;
+pub fn from_column_major(data: Vec<(T, (usize, usize))>, width: usize, height: usize)->self{
+    if data.len() != width * height {
+        panic!("Data length not equal to width * height");
+    }
+}
 
 /*This will return a value at a given position within Array2 */
 pub fn iter_row_major(&self) -> &T;
